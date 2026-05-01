45091d7200c46f3f8249e81e8c68be64b7c3915c
Leonardo <leonardo.estacio.h@gmail.com>
2026-04-30 21:09:46 -0500

    feat: update the main translation unit of pc-2

--- pc-2/src/main.cpp
+++ pc-2/src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+
+#include "solution.h"
 
 using std::cout;
 using std::endl;
+using std::vector;
 
-int main() { cout << "Hello, world!" << endl; }
+int main() {
+  Solution sol;
+
+  vector<int> nums = {10, 1, 2, 4, 7, 2};
+  int limit = 10;
+
+  cout << "optima:   " << sol.longestSubarray(nums, limit) << endl;
+  cout << "ingenua:  " << sol.longestSubarrayNaive(nums, limit) << endl;
+}
